19676a1892562c94bd416043240eb06a443042fa
GamerNoTitle <GamerNoTitle@outlook.com>
2024-05-02 01:50:43 +0800

    fix

--- update-hsr.bat
+++ update-hsr.bat
@@ -2,14 +2,14 @@
 :: Installation Process
 
 :: Launch emulator
-@REM start "MuMu" "E:\Program Files\Netease\MuMuPlayer-12.0\shell\MuMuManager.exe" api -v 0 launch_player
-@REM ping 127.0.0.1 -n 31 > nul
-@REM :: Install new version
-@REM start "MuMu" "E:\Program Files\Netease\MuMuPlayer-12.0\shell\MuMuManager.exe" api -v 0 install_apk hsr.apk
-@REM ping 127.0.0.1 -n 121 > nul
-@REM :: Half an hour to update assets
-@REM start "MuMu" "E:\Program Files\Netease\MuMuPlayer-12.0\shell\MuMuManager.exe" api -v 0 launch_app com.miHoYo.hkrpg
-@REM ping 127.0.0.1 -n 1800 > nul
+start "MuMu" "E:\Program Files\Netease\MuMuPlayer-12.0\shell\MuMuManager.exe" api -v 0 launch_player
+ping 127.0.0.1 -n 31 > nul
+:: Install new version
+start "MuMu" "E:\Program Files\Netease\MuMuPlayer-12.0\shell\MuMuManager.exe" api -v 0 install_apk hsr.apk
+ping 127.0.0.1 -n 121 > nul
+:: Half an hour to update assets
+start "MuMu" "E:\Program Files\Netease\MuMuPlayer-12.0\shell\MuMuManager.exe" api -v 0 launch_app com.miHoYo.hkrpg
+ping 127.0.0.1 -n 1800 > nul
 :: Close the emulator
 start "MuMu" "E:\Program Files\Netease\MuMuPlayer-12.0\shell\MuMuManager.exe" api -v 0 shutdown_player
 del "hsr.apk"
